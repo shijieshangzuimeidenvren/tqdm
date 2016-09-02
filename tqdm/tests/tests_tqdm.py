Fix maxinterval calculation + add unit test
Signed-off-by: Stephen L. <lrq3000@gmail.com>

tqdm/tests/tests_tqdm.py
@@ -9,6 +9,7 @@ import re
 import os
 from nose import with_setup
 from nose.plugins.skip import SkipTest
+from nose.tools import assert_raises
 
 from tqdm import tqdm
 from tqdm import trange
@@ -16,7 +17,7 @@ from tqdm import TqdmDeprecationWarning
 
 try:
     from StringIO import StringIO
-except:
+except ImportError:
     from io import StringIO
 
 from io import IOBase  # to support unicode strings
@@ -367,9 +368,9 @@ def test_max_interval():
     total = 100
     bigstep = 10
     smallstep = 5
-    timer = DiscreteTimer()
 
     # Test without maxinterval
+    timer = DiscreteTimer()
     with closing(StringIO()) as our_file:
         with closing(StringIO()) as our_file2:
             # with maxinterval but higher than loop sleep time
@@ -402,6 +403,7 @@ def test_max_interval():
         assert "25%" not in our_file.read()
 
     # Test with maxinterval effect
+    timer = DiscreteTimer()
     with closing(StringIO()) as our_file:
         with tqdm(total=total, file=our_file, miniters=None, mininterval=0,
                   smoothing=1, maxinterval=1e-4) as t:
@@ -418,6 +420,7 @@ def test_max_interval():
             assert "25%" in our_file.read()
 
     # Test iteration based tqdm with maxinterval effect
+    timer = DiscreteTimer()
     with closing(StringIO()) as our_file:
         with tqdm(_range(total), file=our_file, miniters=None,
                   mininterval=1e-5, smoothing=1, maxinterval=1e-4) as t2:
@@ -432,6 +435,66 @@ def test_max_interval():
 
         our_file.seek(0)
         assert "15%" in our_file.read()
+    
+    # Test different behavior with and without mininterval
+    timer = DiscreteTimer()
+    total = 1000
+    mininterval = 0.1
+    maxinterval = 10
+    with closing(StringIO()) as our_file:
+        with tqdm(total=total, file=our_file, miniters=None, mininterval=mininterval,
+                  smoothing=1, maxinterval=maxinterval) as tm1:
+            with tqdm(total=total, file=our_file, miniters=None, mininterval=0,
+                      smoothing=1, maxinterval=maxinterval) as tm2:
+
+                cpu_timify(tm1, timer)
+                cpu_timify(tm2, timer)
+
+                # Fast iterations, check if dynamic_miniters triggers
+                timer.sleep(mininterval)  # to force update for t1
+                tm1.update(total/2)
+                tm2.update(total/2)
+                assert int(tm1.miniters) == tm2.miniters == total/2
+
+                # Slow iterations, check different miniters if mininterval
+                timer.sleep(maxinterval*2)
+                tm1.update(total/2)
+                tm2.update(total/2)
+                res = [tm1.miniters, tm2.miniters]
+                assert res == [(total/2)*mininterval/(maxinterval*2), (total/2)*maxinterval/(maxinterval*2)]
+
+    # Same with iteratable based tqdm
+    timer1 = DiscreteTimer()  # need 2 timers for each bar because zip not work
+    timer2 = DiscreteTimer()
+    total = 100
+    mininterval = 0.1
+    maxinterval = 10
+    with closing(StringIO()) as our_file:
+        t1 = tqdm(_range(total), file=our_file, miniters=None, mininterval=mininterval,
+              smoothing=1, maxinterval=maxinterval)
+        t2 = tqdm(_range(total), file=our_file, miniters=None, mininterval=0,
+              smoothing=1, maxinterval=maxinterval)
+
+        cpu_timify(t1, timer1)
+        cpu_timify(t2, timer2)
+
+        for i in t1:
+            if i == ((total/2)-2):
+                timer1.sleep(mininterval)
+            if i == (total-1):
+                timer1.sleep(maxinterval*2)
+
+        for i in t2:
+            if i == ((total/2)-2):
+                timer2.sleep(mininterval)
+            if i == (total-1):
+                timer2.sleep(maxinterval*2)
+
+        assert t1.miniters == 0.255
+        assert t2.miniters == 0.5
+
+        t1.close()
+        t2.close()
 
 
 @with_setup(pretest, posttest)
@@ -1233,3 +1296,19 @@ def test_len():
     with closing(StringIO()) as f:
         with tqdm(np.zeros((3, 4)), file=f) as t:
             assert len(t) == 3
+
+
+@with_setup(pretest, posttest)
+def test_deprecation_exception():
+    """Test deprecation custom exception"""
+    def test_TqdmDeprecationWarning():
+        with closing(StringIO()) as our_file:
+            raise (TqdmDeprecationWarning('Test!',
+                                          fp_write=getattr(our_file, 'write',
+                                                           sys.stderr.write)))
+
+    def test_TqdmDeprecationWarning_nofpwrite():
+        raise (TqdmDeprecationWarning('Test!', fp_write=None))
+
+    assert_raises(TqdmDeprecationWarning, test_TqdmDeprecationWarning)
+    assert_raises(Exception, test_TqdmDeprecationWarning_nofpwrite)